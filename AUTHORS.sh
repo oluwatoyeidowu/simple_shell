ef906b61654b23fe85b1945dc3413100bd99c966
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2022-09-07 19:36:12 -0700

    Files

--- AUTHORS.sh
+++ AUTHORS.sh
@@ -3,13 +3,12 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
-# see also ".mailmap" for how email address and names are deduplicated
-
+# see also '.maimap'for how email addresses and names are deduplicated
 {
 	cat <<- 'EOH'
-		# This file list all individuals having contributed content to the repository
-		# For how it is generated, see 'hack/generate-authors.sh'.
+	# This file list all individuals having contributed to the repository.
+	# For how it is generated, see 'hack/generate-authors.sh'.
 	EOH
 	echo
-	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+	git log --format='%an <%aE>' | LC_ALL=C.UTF-8 sort -uf
 } > AUTHORS
